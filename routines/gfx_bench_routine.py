8fd5bf3f937b5f6b2e694c3a4aa0e69cca594c32
MMR3 <johngdovin.0627@gmail.com>
2023-02-25 09:42:16 -0800

    Added button to load test configuration Added test cases to config file.

--- routines/gfx_bench_routine.py
+++ routines/gfx_bench_routine.py
@@ -5,15 +5,20 @@ from adb_cli.adb_comm import ADBCLITool
 class GFXBench:
     """Class method used to run gfx bench Manhattan 3.1"""
 
-    def __init__(self, benchmark, command, serial, target_directory, receiver_directory):
+    def __init__(self, benchmark, serial, test_config, target_directory, receiver_directory):
         """Constructor"""
         self.benchmark = benchmark
-        self.command = command
         self.serial = serial
+        self.test_config = test_config
         self.target_directory = target_directory
         self.receiver_directory = receiver_directory
 
     def execute(self):
         """Method used to run Manhattan Benchmark"""
-        adb_shell = ADBCLITool(self.benchmark, self.command, self.serial,
-                               self.target_directory, self.receiver_directory)
+        if self.benchmark ==  "Manhattan":
+            self.run_manhattan()
+
+    def run_manhattan(self):
+        """Method used to iterate through manhattan benchmark from config"""
+
+
---
+++ routines/send_command_routine.py
@@ -0,0 +1,14 @@
+from adb_cli.adb_comm import ADBCLITool
+
+
+class SendCommand:
+    """Class for sending command to Android Device"""
+
+    def __init__(self, command, serial):
+        self.command = command
+        self.serial = serial
+        self.adb_shell = ADBCLITool(serial=self.serial, command=self.command)
+
+    def send_command(self):
+        """Method used to send command to android device"""
+        self.adb_shell.send_command_to_dut()

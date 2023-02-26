97c652e18632b03db6128730ba90f26b824d6283
MMR3 <johngdovin.0627@gmail.com>
2023-02-25 20:33:30 -0800

    Updated manhattan test case to cycle through test config test case dictionary with list changed adb comm manager to run adb terminal command changed adb comm manager to only instantiate with serial of device

--- routines/gfx_bench_routine.py
+++ routines/gfx_bench_routine.py
@@ -1,6 +1,9 @@
-
+import json
+import time
 
 from adb_cli.adb_comm import ADBCLITool
+from constants import GFXBENCH, MANHATTAN, DISCONNECT
+
 
 class GFXBench:
     """Class method used to run gfx bench Manhattan 3.1"""
@@ -12,13 +15,34 @@ class GFXBench:
         self.test_config = test_config
         self.target_directory = target_directory
         self.receiver_directory = receiver_directory
+        self.adb_shell = None
 
-    def execute(self):
+    def execute_manhattan_test_case(self):
         """Method used to run Manhattan Benchmark"""
-        if self.benchmark ==  "Manhattan":
-            self.run_manhattan()
+        self.load_test_config()
+        self.run_manhattan()
+        self.pull_results()
 
     def run_manhattan(self):
-        """Method used to iterate through manhattan benchmark from config"""
+        """Method used to instantiate Android Debug Bridge Comm Manager"""
+        for test_case in self.test_config[GFXBENCH][MANHATTAN]:
+            command = f'am  broadcast  -a net.kishonti.testfw.ACTION_RUN_TESTS -n ' \
+                      f'net.kishonti.gfxbench.vulkan.v50000.corporate/' \
+                      f'net.kishonti.benchui.corporate.CommandLineSession -e test_ids "{test_case}" '
+            self.adb_shell = ADBCLITool(serial=self.serial)
+            self.adb_shell.send_command_to_dut(command=command)
+        self.adb_shell.send_command_to_dut(DISCONNECT)
 
 
+    def pull_results(self):
+        """Method to pull results from Android Device after running a test"""
+        self.adb_shell = ADBCLITool(serial=self.serial)
+        self.adb_shell.adb_pull_results(target_directory=self.target_directory,
+                                        receiver_directory=self.receiver_directory)
+        self.adb_shell.send_command_to_dut(DISCONNECT)
+
+    def load_test_config(self):
+        """Method used to load test config file"""
+        with open(self.test_config, mode='r', encoding='utf-8') as test_config:
+            self.test_config = json.load(test_config)
+            test_config.close()
